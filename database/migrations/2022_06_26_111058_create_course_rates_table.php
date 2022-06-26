5ae12c2b21f7ac854ed27005e45a81ae5336942d
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-06-26 13:14:03 +0200

    adding migations and models

---
+++ database/migrations/2022_06_26_111058_create_course_rates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_rates', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->bigInteger('course_id')->unsigned()->nullable();
+            $table->integer('rate')->default(1);
+            $table->text('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_rates');
+    }
+}
